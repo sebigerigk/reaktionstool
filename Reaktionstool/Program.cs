fad24ef9fe3d60f4864c3b462a6dd178dc81efd7
Human142857 <65119666+Human142857@users.noreply.github.com>
2021-05-06 12:31:09 +0200

    ens

--- Reaktionstool/Program.cs
+++ Reaktionstool/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Reaktionstool
 {
@@ -10,6 +12,18 @@ namespace Reaktionstool
     {
         static void Main(string[] args)
         {
+            Zaehler stopWatch = new Zaehler();
+            stopWatch.Start();
+            Thread.Sleep(10000);
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
         }
     }
 }
---
+++ Reaktionstool/Zaehler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Diagnostics;
+
+namespace Reaktionstool
+{
+    public class Zaehler
+    {
+
+        Stopwatch sw = new Stopwatch();
+        //Wenn Countdown = 0, dann sw starten
+       
+        //Wenn Taste gedrückt, dann sw = stop
+
+    }
+}
